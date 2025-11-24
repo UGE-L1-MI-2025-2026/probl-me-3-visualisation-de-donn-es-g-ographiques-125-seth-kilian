a3b254c20a7842c94256d941cd4606d61d8d7ec7
SethBelleus-tech <seth.belleus@edu.univ-eiffel.fr>
2025-11-24 12:44:29 +0100

    affichage_77

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from shapefile import *
+from fltk import *
 
+cree_fenetre(1000, 1000)
 sf = Reader("departements-20180101")
 
 print(sf.records())
 
+seine_et_marne = sf.shape(47)
+print(seine_et_marne.bbox)
+
+points = [(i[0]*20, i[1]*20) for i in seine_et_marne.points]
+
+polygone(points, couleur="blue")
+attend_ev()
+ferme_fenetre()
