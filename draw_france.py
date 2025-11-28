e7a91029448f96dcf5b1cd8c2c34bb1c172298a0
SethBelleus-tech <seth.belleus@edu.univ-eiffel.fr>
2025-11-28 12:45:29 +0100

    mercator à finir

--- draw_france.py
+++ draw_france.py
@@ -1,4 +1,5 @@
 import shapefile
+from math import *
 from fltk import * 
 
 def draw_france():
@@ -7,8 +8,19 @@ def draw_france():
 
     for shape_rec in sf.shapeRecords():
         shape = shape_rec.shape
-        points = [(i[0]*15, i[1]*15) for i in shape.points]
+        points = [(i[0]*15+200,i[1]*15-300) for i in shape.points]
         polygone(points, couleur="blue")
 
     attend_ev()
-    ferme_fenetre()
+
+
+def mercator(lon, lat):
+    R = 1000
+    x = R * radians((lon + 180)/360)
+    y = (R/2)-(R/2*pi) * log(tan(pi/4 + radians(lat)/2))
+    return (x, y)
+
+print(mercator(21.2137, 16.2276)) # Example usage of mercator function 
+
+# draw_france()
+# ferme_fenetre()
