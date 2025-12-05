e9fc8005babd4e4a9bb85f8cbfb6f837700fcc81
SethBelleus-tech <seth.belleus@edu.univ-eiffel.fr>
2025-12-05 12:17:53 +0100

    carte_77 réussi (à compléter pour la France)

--- draw_france.py
+++ draw_france.py
@@ -2,45 +2,47 @@ import shapefile
 from math import *
 from fltk import * 
 
-def draw_france():
-    # cree_fenetre(1000, 1000)
-    # sf = shapefile.Reader("departements-20180101")
+def mercator(long, lat):
+    x = long
+    y = degrees(log(tan(radians(lat) / 2 + pi / 4)))
+    return x, y
 
-    # for shape_rec in sf.shapeRecords()[:3]:
-    #     shape = shape_rec.shape
-    #     points = shape.points
-    #     print(points[:10])
-    #     points = [mercator(lon, lat) for lon, lat in points]
-    #     print(points[:10])
-    #     points = [(x + 500, 500 - y) for x, y in points]  # Centering the map
-    #     print(points[:10])
-    #     polygone(points, couleur="blue")
-        
+def calcule_parametres(x_min, y_min, x_max, y_max, X_orig, Y_orig, W, H):
 
-    # attend_ev()
+    a_abscisses = W / (x_max - x_min)
+    a_ordonnees = H / (y_max - y_min)
 
+    a = min(a_abscisses, a_ordonnees)
 
-# def mercator(lon, lat):
-    """x = radians((lon + 180)/360)
-    y = -pi * log(tan(pi/4 + radians(lat)/2))
-    return (x, y)"""
+    B=X_orig - a * x_min
+    C=Y_orig + a * y_max
 
-# print(mercator(81.2137, 6.2276)) Example usage of mercator function 
+    return a, B, C
 
-# draw_france()
-# ferme_fenetre()
-
-sf = shapefile.Reader("departements-20180101")
-print(sf.shape().bbox)
-print(sf.shape(0).bbox)
-print(sf.shape(1).bbox)
-
-x_min = sf.shape().bbox[0]
-y_min = sf.shape().bbox[1]
-
-for i in sf.shape().bbox:
-
+def place_point(x, y, a, B, C):
+    X = a * x + B
+    Y = - a * y + C
+    return X, Y 
+    
 
     
 
-    # return (x_min, y_min, x_max, y_max)
+sf = shapefile.Reader("departements-20180101")
+seine_et_marne = sf.shape(47)
+long_min, lat_min, long_max, lat_max = seine_et_marne.bbox
+
+points_mercator = [mercator(long, lat) for long, lat in seine_et_marne.points]
+
+x_min, y_min = mercator(long_min, lat_min)
+x_max, y_max = mercator(long_max, lat_max)
+
+largeur_fenetre, hauteur_fenetre = 800, 600
+a, B, C = calcule_parametres(x_min, y_min, x_max, y_max, 50, 50, largeur_fenetre, hauteur_fenetre)
+
+point_places = [place_point(x, y, a, B, C) for x, y in points_mercator]
+
+cree_fenetre(1000, 1000)
+polygone(point_places)
+attend_ev()
+ferme_fenetre()
+
